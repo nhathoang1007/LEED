e3f6668c9fd208763b9df0333661776ad22b926b
Jason <jason@enclave.vn>
2016-10-19 17:28:13 +0700

    Jason design login function

--- LearnEnglishEveryday/app/src/main/java/com/example/jason/learnenglisheveryday/activities/BaseActivity.java
+++ LearnEnglishEveryday/app/src/main/java/com/example/jason/learnenglisheveryday/activities/BaseActivity.java
@@ -17,7 +17,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         context = getApplicationContext();
-        ButterKnife.bind(this);
         initView();
     }
 
--- LearnEnglishEveryday/app/src/main/java/com/example/jason/learnenglisheveryday/activities/Login/LoginActivity.java
+++ LearnEnglishEveryday/app/src/main/java/com/example/jason/learnenglisheveryday/activities/Login/LoginActivity.java
@@ -2,9 +2,16 @@ package com.example.jason.learnenglisheveryday.activities.Login;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.jason.learnenglisheveryday.R;
 import com.example.jason.learnenglisheveryday.activities.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by jason on 19/10/2016.
@@ -15,12 +22,18 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void initView() {
     }
 
+
+    @OnClick(R.id.button_normalLogIn)
+    public void loginNormal(){
+        Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show();
+    }
     @Override
     protected void onStart() {
         super.onStart();
